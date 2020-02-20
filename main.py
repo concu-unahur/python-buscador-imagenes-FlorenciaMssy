e1787fe60efc0e43ede45e9b8438cc1d769978ad
FlorenciaMssy <Florencia>
2020-02-19 21:45:15 -0300

    con errores

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message
 carpeta_imagenes = './imagenes'
 
 query = input('¿Imagenes sobre que queres bajar?\n')
-cantidad = int(input(f"¿Que cantidad de imagenes de {query} queres bajar?\n"))
+cantidad = (input(f"¿Que cantidad de imagenes de {query} queres bajar?\n"))
 api = PixabayAPI('15323593-4886427c532904360a1070940', carpeta_imagenes)
 
 logging.info(f'Buscando imagenes de {query}')
